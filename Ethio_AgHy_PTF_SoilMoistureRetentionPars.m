7bf661b2dbdac0e255886ac5d4dba6268deff836
Mosisa Tujuba Wakjira <mosisatujuba@gmail.com>
2025-11-08 12:10:10 +0100

    Update Ethio_AgHy_PTF_SoilMoistureRetentionPars.m

--- Ethio_AgHy_PTF_SoilMoistureRetentionPars.m
+++ Ethio_AgHy_PTF_SoilMoistureRetentionPars.m
@@ -4,19 +4,20 @@
 % Author: Mosisa Tujuba Wakjira
  wp0 = -(0.024*fsand)+(0.487*fclay)+(0.006*fsom+(0.005*(fsand.*fsom)))-...
      (0.013*(fclay.*fsom))+(0.068*(fsand.*fclay))+0.031;
-fWP = wp0+(0.14*wp0)-0.02;
+WP = wp0+(0.14*wp0)-0.02;
  
- % Field capacity (FC) and saturation water content 
+ % Field capacity (FC) and saturation water content (Sat)
  
  fc0 = (-0.251*fsand)+(0.195*fclay)+(0.011*fsom)+(0.006*(fsand.*...
         fsom))-(0.027*(fclay.*fsom))+(0.452*(fsand.*fclay))+0.299;
     
- fFC = fc0 + ((1.283*(fc0.^2))-(0.374*fc0)-0.015);
+ FC = fc0 + ((1.283*(fc0.^2))-(0.374*fc0)-0.015);
  
  st0 = (0.278*fsand)+(0.034*fclay)+(0.022*fsom)-(0.018*(fsand.*...
         fsom))-(0.027*(fclay.*fsom))-(0.584*(fsand.*fclay))+0.078;
     
  st1 = st0+((0.636*st0)-0.107);
     
- fSat = (fFC+st1)+((-0.097*fsand)+0.043);
+ Sat = (FC+st1)+((-0.097*fsand)+0.043);
+
  clear wp0 fc0 st0 st1
